7dd316c619cd5a8fd92899d84aa24d5c3d2f3a37
BaranKarahan <48727345+BaranKarahan@users.noreply.github.com>
2020-05-06 17:13:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace pi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Please give a number as 1st variable: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please give a number as 1st variable: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("The value of the 1st variable: " + a);
+            Console.WriteLine("The value of the 1st variable: " + b);
+
+        }
+    }
+}
